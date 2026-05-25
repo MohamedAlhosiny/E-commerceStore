a0d76d84945831681a3237c861c009895359b070
MohamedAlhosiny <mohamedalsiri110@gmail.com>
2026-05-25 10:37:29 +0300

    So far Auth for  Admin , User complete + SuperAdmin features

--- app/Http/Controllers/api/AdminController.php
+++ app/Http/Controllers/api/AdminController.php
@@ -71,3 +71,5 @@ class AdminController extends Controller
         return $this->successResponse(null, 'Admin deleted successfully', 200);
     }
 }
+// the AUTH for Admin , User finish + SuperAdmin features
+
